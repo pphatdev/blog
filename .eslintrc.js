961b8e44a95fc1231fd06410687b92f1df3adad9
Sophat <sophat.leat@turbotech.com>
2022-12-31 11:50:03 +0700

    Update Formatcode

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
     'react/prop-types': 0,
     'no-unused-vars': 0,
     "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
+    "@next/next/no-page-custom-font": "off",
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
   },
 }
