b336e85c74980454b784597767873a18a2a8e2b0
pphatDev <leatsophat02@gmail.com>
2023-01-08 16:33:03 +0700

    Improvement UI/UX

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -4,6 +4,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { useState } from 'react'
 import Pagination from '@/components/Pagination'
 import formatDate from '@/lib/utils/formatDate'
+import Image from 'next/image'
 
 export default function ListLayout({ posts, title, initialDisplayPosts = [], pagination }) {
   const [searchValue, setSearchValue] = useState('')
@@ -50,31 +51,114 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
         <ul>
           {!filteredBlogPosts.length && 'No posts found.'}
           {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
+            const { slug, date, title, summary, tags, thumbnail, type } = frontMatter
             return (
               <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-3 xl:col-span-3">
-                    <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
-                        </Link>
-                      </h3>
-                      <div className="flex flex-wrap">
+                <article className="flex flex-col gap-5">
+                  <div className="flex w-full flex-col gap-5 sm:flex-row">
+                    <div className="w-full flex-shrink-0 sm:max-w-[15rem]">
+                      {thumbnail ? (
+                        <div className="h-48 w-full overflow-hidden rounded-md border sm:h-full sm:max-h-32 [&>span]:max-h-48 sm:[&>span]:max-h-32 [&>span>img]:w-full [&>span>img]:object-cover">
+                          <Image
+                            className=""
+                            src={thumbnail ?? ''}
+                            width={`512`}
+                            height={`512`}
+                            alt={title}
+                          />
+                        </div>
+                      ) : (
+                        <div className="borde h-full max-h-32 w-full overflow-hidden rounded-md [&>span]:max-h-32 [&>span>img]:w-full [&>span>img]:object-cover">
+                          <div className="flex h-32 w-[15rem] items-center justify-center text-4xl font-bold text-slate-500">
+                            {type == 'music' ? (
+                              <svg
+                                className="h-10 w-10 [&>*]:stroke-slate-400"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22H15C20 22 22 20 22 15Z"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                                <path
+                                  d="M9.62 17.3C10.7908 17.3 11.74 16.3509 11.74 15.1801C11.74 14.0092 10.7908 13.0601 9.62 13.0601C8.44915 13.0601 7.5 14.0092 7.5 15.1801C7.5 16.3509 8.44915 17.3 9.62 17.3Z"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                                <path
+                                  d="M11.74 15.18V7.77002"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                                <path
+                                  d="M13.13 6.7701L15.47 7.55006C16.04 7.74006 16.5 8.38006 16.5 8.98006V9.60005C16.5 10.4101 15.87 10.8601 15.11 10.6001L12.77 9.82008C12.2 9.63008 11.74 8.99009 11.74 8.39009V7.7701C11.74 6.9701 12.36 6.5101 13.13 6.7701Z"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                            ) : type == 'blog' ? (
+                              title[0]
+                            ) : (
+                              <svg
+                                className="h-10 w-10 [&>*]:stroke-slate-400"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M16 2H8C4 2 2 4 2 8V21C2 21.55 2.45 22 3 22H16C20 22 22 20 22 16V8C22 4 20 2 16 2Z"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                                <path
+                                  d="M7 9.5H17"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeMiterlimit="10"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                                <path
+                                  d="M7 14.5H14"
+                                  stroke="#292D32"
+                                  strokeWidth="1.5"
+                                  strokeMiterlimit="10"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                />
+                              </svg>
+                            )}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                    <div className="w-full">
+                      <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                        <h1 className="text-2xl font-bold leading-8 tracking-tight">{title}</h1>
+                      </Link>
+                      <div>
                         {tags.map((tag) => (
                           <Tag key={tag} text={tag} />
                         ))}
                       </div>
-                    </div>
-                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                      {summary}
+                      <div className="prose max-w-none text-gray-500 line-clamp-2 dark:text-gray-400">
+                        {summary}
+                      </div>
+                      <time className="text-xs font-medium text-slate-600" dateTime={date}>
+                        {formatDate(date)}
+                      </time>
                     </div>
                   </div>
                 </article>
