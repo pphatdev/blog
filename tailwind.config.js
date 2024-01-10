54dbe2f91d6e5753c8f5179fe9399d7e4f4e92bf
pphatdev <leatsophat02@gmail.com>
2024-01-10 11:04:51 +0700

    10012924

--- tailwind.config.js
+++ tailwind.config.js
@@ -165,7 +165,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 }
